ceb082e08ca3a6c6c21a1411468281fc4eb1e6b4
Ahmed Abdalla <ahmed.abdalla@avant.com>
2015-10-26 14:19:35 -0500

    sets up base honeybadger payload with sprintf

--- R/log.R
+++ R/log.R
@@ -10,7 +10,25 @@ with_logging <- function(expr) {
 parse_error <- function(error) {
   trace <- bettertrace::stacktrace()
 
-  honey_badger_payload = list(backtrace = error)
-  parsed_honey_badger_payload <- rjson::toJSON(honey_badger_payload)
-  print(parsed_error)
+  class <- "foo"
+  message <- "someError"
+  tags <- list("foo", "bar")
+  backtrace <- rjson::toJSON(trace)
+
+  honeybadger_payload =  sprintf('{
+"notifier":{
+  "name":"honeylogging Notifier",
+    "url":"TBD",
+    "version":"1.0.0"
+},
+"error": {
+  "class": %s,
+  "message": %s,
+  "tags": %s,
+  },
+ "backtrace": %s}', class, message, tags, backtrace)
+
+  # list(backtrace = error)
+  # parsed_honey_badger_payload <- rjson::toJSON(honey_badger_payload)
+  print(honeybadger_payload)
 }
