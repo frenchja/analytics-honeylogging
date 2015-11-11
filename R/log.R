c834a2203529e307d192b8e5a67a92b354fc08b5
Ahmed Abdalla <ahmed.abdalla@avant.com>
2015-11-11 11:16:11 -0600

    comments code; adds ftn doc header

--- R/log.R
+++ R/log.R
@@ -1,5 +1,5 @@
-#' Wrap an expression with this function, and on error it will POST
-#' error stacktrace to honeybadger
+#' Wrap an expression with this function, if the expression errors, the error and stack
+#' trace will be POSTed to honeybadger
 #'
 #' @param expr expression. Expression to be evaluated.
 #' @param message character. Message to be displayed on henoeybadger for this error.
@@ -12,20 +12,16 @@ with_logging <- function(expr, message = "NA", tags = list()) {
   )
 }
 
-parse_error <- function(error) {
-  trace <- stacktrace()
-  signalCondition(error)
 
-  trace_output <- lapply(trace, function(element) {
-    # Turn unevaluated expressions into character strings and then concatenate.
-    element$method <- paste(collapse = " ", deparse(width.cutoff = 500L, element$method))
-    element
-  })
-  trace_output
-}
-
+#' Log an error to honeybadger. The error's stack trace will be parsed for you.
+#'
+#' @param error expression. Error object.
+#' @param message character. Message to be displayed on henoeybadger for this error.
+#' @param tags list. Additional tags for sorting on honeybadger UI.
+#' @export
 log_error <- function(error, message = "NA", tags = list()) {
-  # Get stacktrace if we have an error
+  # Get stacktrace if we have an error. If no error is provided, honeybadger's
+  # backtrace array is instead an empty list
   if (is.null(error)) {
     backtrace <- list()
   } else {
@@ -35,7 +31,7 @@ log_error <- function(error, message = "NA", tags = list()) {
   honeybadger_payload <- list(
     notifier = list(
       name = "honeylogging Notifier",
-      url = "TBD"  # TODO
+      url = "TBD"  # NOTE(Ahmed): This is useful in the web context; aka logging in Rails.
     ),
     error = list(
       # TODO: What qualifies as a class for R?
@@ -65,6 +61,19 @@ log_error <- function(error, message = "NA", tags = list()) {
   )
 
   post_to_honeybadger(honeybadger_payload)
+}
+
+
+parse_error <- function(error) {
+  trace <- stacktrace()
+  signalCondition(error)
+
+  trace_output <- lapply(trace, function(element) {
+    # Turn unevaluated expressions into character strings and then concatenate.
+    element$method <- paste(collapse = " ", deparse(width.cutoff = 500L, element$method))
+    element
+  })
+  trace_output
 }
 
 
